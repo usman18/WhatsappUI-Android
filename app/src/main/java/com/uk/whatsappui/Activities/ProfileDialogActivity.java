7cac55cdce8eb09282bfc74d3b41215a319f0f2e
usman18 <secretdeveloper18@gmail.com>
2019-07-01 22:33:15 +0530

    Gifs added

--- app/src/main/java/com/uk/whatsappui/Activities/ProfileDialogActivity.java
+++ app/src/main/java/com/uk/whatsappui/Activities/ProfileDialogActivity.java
@@ -9,17 +9,17 @@ import com.bumptech.glide.request.RequestOptions;
 import com.uk.whatsappui.R;
 
 public class ProfileDialogActivity extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_dialog);
-
-        ImageView profileImage=findViewById(R.id.profile_image);
-        Glide.with(this)
-                .load(getIntent().getStringExtra("url"))
-                .apply(new RequestOptions().placeholder(R.drawable.profile))
-                .into(profileImage);
-
-    }
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile_dialog);
+		
+		ImageView profileImage = findViewById(R.id.profile_image);
+		Glide.with(this)
+			.load(getIntent().getStringExtra("url"))
+			.apply(new RequestOptions().placeholder(R.drawable.profile))
+			.into(profileImage);
+		
+	}
 }
