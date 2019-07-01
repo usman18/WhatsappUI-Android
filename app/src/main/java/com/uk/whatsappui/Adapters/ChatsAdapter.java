0e8ff792d8b20d046f13b21ed75f94da7ab034d1
Shashank Chandak <chandak.shashank16@gmail.com>
2019-07-01 17:48:53 +0530

    Shared Element transition animation completed

--- app/src/main/java/com/uk/whatsappui/Adapters/ChatsAdapter.java
+++ app/src/main/java/com/uk/whatsappui/Adapters/ChatsAdapter.java
@@ -1,6 +1,9 @@
 package com.uk.whatsappui.Adapters;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.uk.whatsappui.Activities.ProfileDialogActivity;
 import com.uk.whatsappui.Model.Chat;
 import com.uk.whatsappui.R;
 
@@ -80,6 +84,17 @@ public class ChatsAdapter extends RecyclerView.Adapter<ChatsAdapter.ChatsViewHol
 			tvMsg = itemView.findViewById(R.id.tvLastMsg);
 			tvTime = itemView.findViewById(R.id.tvTime);
 			chatLayoutContainer = itemView.findViewById(R.id.chat_row_container);
+
+			profilePic.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					Intent i = new Intent(new Intent(context, ProfileDialogActivity.class));
+					i.putExtra("url", chats.get(getAdapterPosition()).getImage());
+					ActivityOptions options = ActivityOptions
+							.makeSceneTransitionAnimation((Activity)context, view, "transition");
+					context.startActivity(i, options.toBundle());
+				}
+			});
 		}
 	}
 }
